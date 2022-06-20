8d0a52bb23c7ba245ccdd3a4716abbcd7d3c430b
shurintou <shurintou@gmail.com>
2022-06-20 19:36:57 +0900

    chore(code split): to modify the build shell to overwrite source name of all new chunk and file.

--- build.sh
+++ build.sh
@@ -1,3 +1,10 @@
+function getHash(){
+    filename=$1
+    tempFilename=${filename#*.}
+    filehash=${tempFilename#*.}
+    echo "$filehash"
+}
+
 function build(){
     for dir in $1/static/*
         do
@@ -9,15 +16,19 @@ function build(){
             then
                 for sourceFile in $1/static/$dirPath/*
                     do
-                    sourceFilename=${sourceFile##*/}
-                    sourceFiletype=${sourceFilename##*.}
+                    sourceFullFilename=${sourceFile##*/} # main.50e7c4e3.js
+                    sourceFilename=${sourceFullFilename%%.*} # main
+                    sourceHash="getHash $sourceFullFilename"  # 50e7c4e3
+                    sourceFiletype=${sourceFullFilename##*.} # js
                     for targetFile in static/$dirPath/*
                         do
-                        targetFilename=${targetFile##*/}
-                        targetFiletype=${targetFilename##*.}
-                        if [[ ${targetFilename} != ${sourceFilename} && ${sourceFiletype} == ${targetFiletype} ]]
+                        targetFullFilename=${targetFile##*/}
+                        targetFilename=${targetFullFilename%%.*}
+                        targetHash="getHash $targetFullFilename"
+                        targetFiletype=${targetFullFilename##*.}
+                        if [[ ${targetFilename} == ${sourceFilename} && ${sourceFiletype} == ${targetFiletype} && ${targetHash} != ${sourceHash} ]]
                         then
-                            git mv ${targetFile} static/$dirPath/${sourceFilename}
+                            git mv ${targetFile} static/$dirPath/${sourceFullFilename}
                             break
                         fi
                     done
